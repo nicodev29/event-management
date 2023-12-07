eea595d7a6e5b5ac7e046407d4dac59e05b31afd
Nicolas Garcia <zextnigarcia@correoargentino.com.ar>
2023-12-07 19:00:24 -0300

    updates

--- front-event-manager/src/app/app.component.spec.ts
+++ front-event-manager/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tennis-front-app'`, () => {
+  it(`should have as title 'wise-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tennis-front-app');
+    expect(app.title).toEqual('wise-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tennis-front-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('wise-app');
   });
 });
